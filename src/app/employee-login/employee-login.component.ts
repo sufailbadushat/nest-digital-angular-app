d4887cc8964333921e2f2cfbf56b56ba0c157abb
sufailbadushat <sufailbadushat@gamil.com>
2022-11-30 20:21:49 +0530

    route set and authentication

--- src/app/employee-login/employee-login.component.ts
+++ src/app/employee-login/employee-login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-employee-login',
@@ -6,5 +7,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./employee-login.component.css']
 })
 export class EmployeeLoginComponent {
+  employeeId = ""
+  password = ""
+
+  constructor(private route:Router){}
+
+  readValue = () => {
+    let data: any = {
+      "employeeId": this.employeeId,
+      "password": this.password
+    }
+    console.log(data);
+    if (this.employeeId=="1122" && this.password=="12345") {
+      this.route.navigate(['/addCourses'])
+    } else {
+      alert("invalid login..!")
+    }
+  }
 
 }
